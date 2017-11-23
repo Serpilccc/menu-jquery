c62476dc9c082fa7b86ee3a9b71b1b46a82feb38
serpilcc <serpil60@hotmail.fr>
2017-11-23 20:03:52 +0100

    css

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,26 @@
+//('#toggle').click(function(){
+   // $('#a').toggle();
+    //$('#b').toggle(1000);
+    //$('#c').toggle('slow');
+    //$('#d').toggle('slow', function(){
+        //console.log('Élément #d est caché/affiché');
+    //});
+//});//
+
+
+
+
+$('presentation').click(function(){
+    $('.pres').toggle();
+});
+
+$('plats').click(function(){
+    $('#menu').toggle();
+});
+
+$('tarif').click(function(){
+    $('#tarifs').toggle();
+});
+
+
+
